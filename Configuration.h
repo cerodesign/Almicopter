45227d84fbce168e1890fb41034580090d8190b6
FanatiKKK <michal.vasko3@gmail.com>
2016-11-19 22:02:48 +0100

    Configuration file

---
+++ Configuration.h
@@ -0,0 +1,26 @@
+//PID
+#define PID_PITCH_MIN -255
+#define PID_PITCH_MAX 255
+
+#define PID_ROLL_MIN -255
+#define PID_ROLL_MAX 255
+
+#define PID_YAW_MIN -255
+#define PID_YAW_MAX 255
+
+//Motory
+#define MOTOR_ZERO_LEVEL 700
+
+//LEDky
+#define LED_PIN 13
+
+//uhly
+#define ROLL_MAX_ANGLE 30
+#define ROLL_MIN_ANGLE -30
+
+#define PITCH_MAX_ANGLE 30
+#define PITCH_MIN_ANGLE -30
+
+#define YAW_MAX_ANGLE 180
+#define YAW_MIN_ANGLE -180
+
